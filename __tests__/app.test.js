de557015be622479ada0e0a1e17ed36933f09507
Pedro <pedro.chn01@gmail.com>
2022-08-03 16:06:26 -0700

    first test started and failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,52 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('#GET /jerseys should return a list of jerseys', async () => {
+    const resp = await request(app).get('/jerseys');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual([
+      {
+        id: '1',
+        name: 'Real Madrid FC',
+        country: 'Spain',
+        league: 'La Liga Santander',
+        cost: '105'
+      },
+      {
+        id: '2',
+        name: 'Manchester United FC',
+        country: 'England',
+        league: 'Barclays English Preimer Leauge',
+        cost: '90'
+      },
+      {
+        id: '3',
+        name: 'Juventus CF',
+        country: 'Italy',
+        league: 'Seria A',
+        cost: '93'
+      },
+      {
+        id: '4',
+        name: 'PSG',
+        country: 'France',
+        league: 'league un',
+        cost: '85'
+      },
+      {
+        id: '5',
+        name: 'Timbers',
+        country: 'USA',
+        league: 'MLS',
+        cost: '500'
+      },
+    ]);
   });
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,43 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS jerseys;
+
+CREATE TABLE jerseys (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    country VARCHAR NOT NULL,
+    league VARCHAR NOT NULL,
+    cost BIGINT
+);
+
+INSERT INTO 
+    jerseys (name, country, league, cost)
+VALUES
+    (
+        'Real Madrid FC',
+        'Spain',
+        'La Liga Santander',
+        '105'
+    ),
+    (
+        'Manchester United FC',
+        'England',
+        'Barclays English Preimer Leauge',
+        '90'
+    ),
+    (
+        'Juventus CF',
+        'Italy',
+        'Seria A',
+        '93'
+    ),(
+        'PSG',
+        'France',
+        'League un',
+        '85'
+    ),(
+        'Timbers',
+        'USA',
+        'MLS',
+        '500'
+    );
