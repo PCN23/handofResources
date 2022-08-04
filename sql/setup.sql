2eff0f4c38b014afb95f72e9ac2bed58800fdb3e
Pedro <pedro.chn01@gmail.com>
2022-08-04 15:59:07 -0700

    added 2nd set of info in SQL table

--- sql/setup.sql
+++ sql/setup.sql
@@ -9,9 +9,51 @@ CREATE TABLE jerseys (
     league VARCHAR NOT NULL,
     cost BIGINT
 );
+CREATE TABLE cereals (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    maker VARCHAR NOT NULL,
+    price BIGINT,
+    size VARCHAR NOT NULL
+);
 
 INSERT INTO 
+    cereals (name, maker, price, size)
+
+VALUES
+(
+    'Carlos V',
+    'Nestel',
+    '18',
+    'large'
+),
+(
+    'ChocaPic',
+    'Nestel',
+    '25',
+    'large'
+),
+(
+    'Pelotitas',
+    'Gonely',
+    '36',
+    'extra large'
+),
+(
+    'kariot',
+    'Telma',
+    '14',
+    'medium'
+),
+(
+    'Krispy cream os',
+    'kellog',
+    '19',
+    'small'
+);
+INSERT INTO 
     jerseys (name, country, league, cost)
+    
 VALUES
     (
         'Real Madrid FC',
