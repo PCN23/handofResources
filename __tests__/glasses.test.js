69f4e818eb8a60edc302bd60ca3cbd34850490a4
Pedro <pedro.chn01@gmail.com>
2022-08-05 17:01:58 -0700

    3rd test passing in 4th set of info

--- __tests__/glasses.test.js
+++ __tests__/glasses.test.js
@@ -9,7 +9,7 @@ describe('backend-express-template routes', () => {
   });
   it('#GET /glasses should return a list of glasses', async () => {
     const resp = await request(app).get('/glasses');
-    //expect(resp.status).toBe(200);
+    expect(resp.status).toBe(200);
     expect(resp.body).toEqual([
       {
         id: '1',
@@ -33,6 +33,31 @@ describe('backend-express-template routes', () => {
         color: 'yellow',
       },
     ]);
+  });
+  it('#GET glasses/:id should return a single glasses', async () => {
+    const resp = await request(app).get('/glasses/2');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      id: '2',
+      name: 'lampwork',
+      glassblower: 'Jim Aden',
+      gas_used: 'propane',
+      color: 'baby blue',
+    });
+  });
+  it('#POST /glasses should create a new glasses', async () => {
+    const newGlass = {
+      name: 'Roadglass', 
+      glassblower: 'james', 
+      gas_used: 'propane',
+      color: 'orange',
+    };
+    const resp = await request(app).post('/glasses').send(newGlass);
+    //expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      ...newGlass,
+    });
   });
   afterAll(() => {
     pool.end();
